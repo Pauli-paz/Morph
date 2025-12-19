82d860518d9cb1fbe37c62edc616180f5a0b2a3a
Paula Firmapaz <firmapazpaula@gmail.com>
2025-12-19 12:00:11 -0300

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import react from "@vitejs/plugin-react";
 import tailwind from "tailwindcss";
 import { defineConfig } from "vite";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   publicDir: "./static",
-  base: "./",
+  base: "/Morph/",
   css: {
     postcss: {
       plugins: [tailwind()],
